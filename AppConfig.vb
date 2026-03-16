6ae7414d0e21e18d98298825313fe8ef1a0db122
hhetari-masfoam <hhetari@gmail.com>
2026-03-16 23:41:57 +0300

    Upload project

--- AppConfig.vb
+++ AppConfig.vb
@@ -1,9 +1,16 @@
 ﻿Imports System.Configuration
+Imports System.ComponentModel
 
 Public Module AppConfig
 
     Public ReadOnly Property MainConnectionString As String
         Get
+            ' WinForms designer loads forms at design-time
+            If LicenseManager.UsageMode = LicenseUsageMode.Designtime Then
+                ' Return a harmless placeholder; it should never be used to actually connect.
+                Return "Data Source=(local);Initial Catalog=master;Integrated Security=True;"
+            End If
+
             Dim cs = ConfigurationManager.ConnectionStrings("MainDB")
 
             If cs Is Nothing OrElse String.IsNullOrWhiteSpace(cs.ConnectionString) Then
